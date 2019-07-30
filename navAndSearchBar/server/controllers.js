9ea182e563185ab481ed310e3bbad619cfa9a545
jjklee <jjlee720@gmail.com>
2019-07-29 19:45:15 -0700

    [feat] adds search by name

--- navAndSearchBar/server/controllers.js
+++ navAndSearchBar/server/controllers.js
@@ -1,14 +1,16 @@
 const { findById, findByName } = require('../db-mongo/mHelper.js');
 
-const search = (req, res) => {
-  console.log('in controllers', req.query.query)
-  findById()
+const searchById = (req, res) => {
+  let id = req.query.query
+  findById(id)
     .then((result) => res.status(200).send(result))
     .catch((err) => res.status(404).send(err))
-  
-  // findByName(req.query.query)
-  //   .then((result) => res.status(200).send(result))
-  //   .catch((err) => res.status(404).send(err))
 }
 
-module.exports = search;
+const searchByName = (req, res) => {
+  findByName(req.query.query)
+    .then((result) => res.status(200).send(result))
+    .catch((err) => res.status(404).send(err))
+}
+
+module.exports = { searchById , searchByName };
