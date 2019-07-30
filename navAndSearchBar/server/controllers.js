ba3fc32b72be6eb573d89940e101675952a73630
jjklee <jjlee720@gmail.com>
2019-07-30 16:49:32 -0700

    [feat] utilizes postgresql database

--- navAndSearchBar/server/controllers.js
+++ navAndSearchBar/server/controllers.js
@@ -1,16 +1,18 @@
-const { findById, findByName } = require('../db-mongo/mHelper.js');
+// const { findById, findByName } = require('../db-mongo/mHelper.js');
+const { findById, findByName } = require('../db-pg/pHelper.js');
 
 const searchById = (req, res) => {
-  let id = req.query.query
+  let id = req.query.query;
   findById(id)
     .then((result) => res.status(200).send(result))
     .catch((err) => res.status(404).send(err))
 }
 
 const searchByName = (req, res) => {
+  let query = req.query.query;
   findByName(req.query.query)
     .then((result) => res.status(200).send(result))
     .catch((err) => res.status(404).send(err))
 }
 
-module.exports = { searchById , searchByName };
+module.exports = { searchById, searchByName};
