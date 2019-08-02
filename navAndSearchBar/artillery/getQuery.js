d44dca5b1b2f18ecae420b513e1f1c230ea0a972
jjklee <jjlee720@gmail.com>
2019-08-02 16:23:21 -0700

    [feat] increases arrival rates

--- navAndSearchBar/artillery/getQuery.js
+++ navAndSearchBar/artillery/getQuery.js
@@ -1,4 +1,4 @@
-const { categories } = require('../db-mongo/dataGen/dataGen.json');
+const { categories } = require('../db-mongo/dataGen/dataGen.json.js');
 
 function setupSomeData(context, events, done) {
   let q = categories[Math.floor(Math.random() * 25)];
