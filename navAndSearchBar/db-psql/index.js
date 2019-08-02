f822ff266f34ae7b5b62ce3d586f3b8ccec68485
jjklee <jjlee720@gmail.com>
2019-08-02 16:26:22 -0700

    [feat] updates to use pool

--- navAndSearchBar/db-psql/index.js
+++ navAndSearchBar/db-psql/index.js
@@ -1,9 +1,3 @@
-// var { Client } = require('pg');
-// const connectionString = 'postgres://jjlee@localhost:5432/sdc';
-// const client = new Client({ connectionString });
-
-// module.exports = client;
-
 var { Pool } = require('pg');
 const connectionString = 'postgres://jjlee@localhost:5432/sdc';
 const pool = new Pool({ connectionString });
