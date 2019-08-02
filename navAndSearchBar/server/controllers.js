a29ff93e44d446435b7bf6a0d79e9b55763ab2d0
jjklee <jjlee720@gmail.com>
2019-08-01 17:09:35 -0700

    [refactory] separates posgresql from mongo controllers

--- navAndSearchBar/server/controllers.js
+++ navAndSearchBar/server/controllers.js
@@ -1,6 +1,27 @@
-const { findById, findByNameText, findByNameRegex } = require('../db-mongo/mHelper.js');
-// const { findById, findByName } = require('../db-pg/pHelper.js');
+/* MONGO */
+// const { findById, findByNameText, findByNameRegex } = require('../db-mongo/mHelper.js');
 
+// const searchById = (req, res) => {
+//   findById(req.query.query)
+//     .then((result) => res.status(200).send(result))
+//     .catch((err) => res.status(404).send(err))
+// }
+
+// const searchByName = (req, res) => {
+//   findByNameText(req.query.query)
+//     .then(result => {
+//       if (result.length < 1) {
+//         findByNameRegex(req.query.query)
+//           .then(products => res.status(200).send(products))
+//       } else {
+//         res.status(200).send(result)
+//       }
+//     })
+//     .catch((err) => res.status(404).send(err))
+// }
+
+/* POSTGRESQL */
+const { findById, findByName } = require('../db-pg/pHelper.js');
 const searchById = (req, res) => {
   findById(req.query.query)
     .then((result) => res.status(200).send(result))
@@ -8,16 +29,9 @@ const searchById = (req, res) => {
 }
 
 const searchByName = (req, res) => {
-  findByNameText(req.query.query)
-    .then(result => {
-      if (result.length < 1) {
-        findByNameRegex(req.query.query)
-          .then(products => res.status(200).send(products))
-      } else {
-        res.status(200).send(result)
-      }
-    })
-    .catch((err) => res.status(404).send(err))
+  findByName(req.query.query)
+  .then(result => res.status(200).send(result))
+  .catch((err) => res.status(404).send(err));
 }
 
 module.exports = { searchById, searchByName};
