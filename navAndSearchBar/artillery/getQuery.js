3af91c7dcaf55d2025cebf596aca0c73c4a3afe9
jjklee <jjlee720@gmail.com>
2019-08-01 17:37:38 -0700

    [ops] renames files

--- navAndSearchBar/artillery/getQuery.js
+++ navAndSearchBar/artillery/getQuery.js
@@ -1,4 +1,4 @@
-const { categories } = require('./db-mongo/dataGen/dataGen.json');
+const { categories } = require('../db-mongo/dataGen/dataGen.json');
 
 function setupSomeData(context, events, done) {
   let q = categories[Math.floor(Math.random() * 25)];
--- navAndSearchBar/db-pg/index.js
+++
@@ -1,5 +0,0 @@
-var { Pool, Client } = require('pg');
-const connectionString = 'postgres://jjlee@localhost:5432/sdc';
-const client = new Client({ connectionString });
-
-module.exports = client;
