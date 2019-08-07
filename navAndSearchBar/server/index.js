d5f6c3466019fe70486edf023b803edbd0d4e563
jjklee <jjlee720@gmail.com>
2019-08-07 10:07:34 -0700

    [fix] loader.io routing

--- navAndSearchBar/server/index.js
+++ navAndSearchBar/server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = require('./router.js');
 const cors = require('cors');
-const port = process.env.PORT || 2001;
 
+const port = process.env.PORT || 2001;
 const app = express();
 
 app.use(express.json());
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/api', router);
-app.use('/loaderio-f357707ebcd3743558a61f84e307a372', (err, res) => {
-  if (err) res.status(404).send(err);
+app.use('/loaderio-f357707ebcd3743558a61f84e307a372', (req, res) => {
   res.send('loaderio-f357707ebcd3743558a61f84e307a372');
 })
+
 app.listen(port, () => console.log(`Listening on ${port}, smooth jazz`));
