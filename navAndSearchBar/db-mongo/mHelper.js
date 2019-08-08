c034f18745d9adcbd5df9d1462315220ff999658
jjklee <jjlee720@gmail.com>
2019-08-08 10:42:38 -0700

    [style] removes white space

--- navAndSearchBar/db-mongo/mHelper.js
+++ navAndSearchBar/db-mongo/mHelper.js
@@ -5,17 +5,14 @@ const findById = (id) => {
  }
 
 const findByNameText = (query) => {
-  // return Search.find({$or: [{$text: {$search: query}}, {name: {$regex: query, $options: 'i'}}]}).limit(24);
-  //  `"\"${query}\""`
-
+  
   return Search.find({$text: { $search: query }}).limit(24)
 }
 
 const findByNameRegex = (query) => {
   const searchString = new RegExp(query, `i`);
   return Search.find({ name: searchString }).limit(24)
-  
-  // return Search.find({ name: { $regex: query, $options: 'i' }}).limit(24)
+  // return Search.find({ name: { $regex: query, $options: 'i' }}).limit(24);
 }
 
 module.exports = { findById, findByNameText, findByNameRegex };
